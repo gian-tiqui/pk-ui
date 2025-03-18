0544de64ca2899644da5c883492a965832395d09
gian-tiqui <gian.tiqui.dev@gmail.com>
2025-03-18 10:14:49 +0800

    removed unused vars

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -7,7 +7,7 @@ import { Link, useNavigate } from "react-router-dom";
 import PageTemplate from "../templates/PageTemplate";
 import { useEffect, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
-import { Namespace, URI } from "../@utils/enums/enum";
+import { Namespace } from "../@utils/enums/enum";
 import { Toast } from "primereact/toast";
 import handleLoginError from "../@utils/functions/handleErrors";
 import Cookies from "js-cookie";
