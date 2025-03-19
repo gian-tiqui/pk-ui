9ed845a48b4e225556df5829f3a5ea163334dbe5
gian-tiqui <gian.tiqui.dev@gmail.com>
2025-03-19 13:47:38 +0800

    added back button to qmeup component

--- src/pages/QmeupPage.tsx
+++ src/pages/QmeupPage.tsx
@@ -1,9 +1,21 @@
+import { Button } from "primereact/button";
 import PageTemplate from "../templates/PageTemplate";
+import { useNavigate } from "react-router-dom";
+import { PrimeIcons } from "primereact/api";
 
 const QmeupPage = () => {
+  const navigate = useNavigate();
+
   return (
     <PageTemplate>
-      <div className="w-full h-screen">
+      <div className="relative w-full h-screen">
+        <Button
+          className="absolute w-10 h-10 top-10 left-10"
+          onClick={() => {
+            navigate(-1);
+          }}
+          icon={`${PrimeIcons.BACKWARD}`}
+        ></Button>
         <iframe
           className="w-full h-full"
           src="https://qmeup.westlakemed.com.ph:9096/signin"
